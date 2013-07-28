b130705b22ce856022aa8bb7d4d11d5df960e5ad
DSX <druseer@ubuntu.ubuntu-domain>
2013-07-28 19:18:58 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = 'fd1349965699ca1f057d7048f73bfe8901c6c82e1f946bc0257c181c41d8669eb7ae892b525962cc5e494cc90783a1857a5fa3a9d62709fba3e0f61793e3b3a1'
